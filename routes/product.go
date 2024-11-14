9e679fdb76f370c63a83862b49408df8f1d73b20
harcop <harcopdrew@gmail.com>
2024-11-14 18:57:29 +0000

    fix product post route

--- routes/product.go
+++ routes/product.go
@@ -8,5 +8,5 @@ import (
 
 func ProductRoutes(r *mux.Router) {
 	r.HandleFunc("/products", controllers.GetProducts).Methods("GET")
-	r.HandleFunc("/proudcts", controllers.CreateProduct).Methods("POSt")
+	r.HandleFunc("/products", controllers.CreateProduct).Methods("POST")
 }
