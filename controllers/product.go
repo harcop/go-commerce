de4c9dcc6ce08f069ddb7987b844693b67630254
harcop <harcopdrew@gmail.com>
2024-11-14 21:25:47 +0000

    Add single product get

--- controllers/product.go
+++ controllers/product.go
@@ -3,10 +3,35 @@ package controllers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"ecommerce-backend/database"
 	"ecommerce-backend/models"
+
+	"github.com/gorilla/mux"
 )
+
+func GetProduct(w http.ResponseWriter, r *http.Request) {
+	var product models.Product
+	var productID string
+
+	vars := mux.Vars(r)
+	if id, exists := vars["id"]; exists {
+		productID = id
+	} else {
+		http.Error(w, "product id is requiresd", http.StatusBadRequest)
+		return
+	}
+
+	id, _ := strconv.Atoi(productID)
+
+	if err := database.DB.First(&product, id).Error; err != nil {
+		http.Error(w, "Product not found", http.StatusNotFound)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(product)
+}
 
 func GetProducts(w http.ResponseWriter, r *http.Request) {
 	var products []models.Product
--- routes/product.go
+++ routes/product.go
@@ -9,4 +9,5 @@ import (
 func ProductRoutes(r *mux.Router) {
 	r.HandleFunc("/products", controllers.GetProducts).Methods("GET")
 	r.HandleFunc("/products", controllers.CreateProduct).Methods("POST")
+	r.HandleFunc("/product/{id}", controllers.GetProduct).Methods("GET")
 }
